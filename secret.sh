79f11409aebe180fd2300f7be13a1d9a889c293c
dhanabaloptisol <122263933+dhanabaloptisol@users.noreply.github.com>
2024-05-27 16:56:06 +0530

    Create secret.sh

---
+++ secret.sh
@@ -0,0 +1,21 @@
+ACCOUNT=882027629355
+REGION=us-east-1
+SECRET_NAME=aws-secret
+EMAIL=email@email.com ( can be anything)
+
+#
+# Fetch token (which will expire in 12 hours)
+#
+
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+
+#
+# Create or replace registry secret
+#
+
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME \
+ --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
+ --docker-username=AWS \
+ --docker-password="${TOKEN}" \
+ --docker-email="${EMAIL}"
